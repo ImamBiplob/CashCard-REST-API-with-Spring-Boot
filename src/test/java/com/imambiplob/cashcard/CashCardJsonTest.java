4648bfd575603cb694a7f43bdccee66226f9aba0
ImamBiplob <imamhbiplob@gmail.com>
2023-06-20 01:09:42 +0600

    add Spring Security and implement Authentication

--- src/test/java/com/imambiplob/cashcard/CashCardJsonTest.java
+++ src/test/java/com/imambiplob/cashcard/CashCardJsonTest.java
@@ -1,6 +1,5 @@
 package com.imambiplob.cashcard;
 
-
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,9 +25,9 @@ public class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00));
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1"));
     }
 
     @Test
@@ -48,55 +47,31 @@ public class CashCardJsonTest {
         String expected = """
                 {
                     "id": 99,
-                    "amount": 123.45
+                    "amount": 123.45,
+                    "owner": "sarah1"
                 }
                 """;
         assertThat(json.parse(expected))
-                .isEqualTo(new CashCard(99L, 123.45));
-        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+                .isEqualTo(new CashCard(99L, 123.45, "sarah1"));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 
     @Test
-void cashCardListSerializationTest() throws IOException {
-   assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
-}
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
 
-@Test
-void cashCardListDeserializationTest() throws IOException {
-   String expected="""
-         [
-            { "id": 99, "amount": 123.45 },
-            { "id": 100, "amount": 1.00 },
-            { "id": 101, "amount": 150.00 }
-         ]
-         """;
-   assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
-}
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expected = """
+                [
+                     {"id": 99, "amount": 123.45 , "owner": "sarah1"},
+                     {"id": 100, "amount": 1.00 , "owner": "sarah1"},
+                     {"id": 101, "amount": 150.00, "owner": "sarah1"}
 
-@Test
-void shouldReturnASortedPageOfCashCards() {
-    ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1&sort=amount,desc", String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-    DocumentContext documentContext = JsonPath.parse(response.getBody());
-    JSONArray read = documentContext.read("$[*]");
-    assertThat(read.size()).isEqualTo(1);
-
-    double amount = documentContext.read("$[0].amount");
-    assertThat(amount).isEqualTo(150.00);
-}
-
-@Test
-void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
-    ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-    DocumentContext documentContext = JsonPath.parse(response.getBody());
-    JSONArray page = documentContext.read("$[*]");
-    assertThat(page.size()).isEqualTo(3);
-
-    JSONArray amounts = documentContext.read("$..amount");
-    assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
-}
+                ]
+                """;
+        assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
+    }
 }
